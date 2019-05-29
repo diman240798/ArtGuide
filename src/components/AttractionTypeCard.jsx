c2252bc65eee1b41b9b980481f143aaad02ace36
diman <diman@mail.ru>
2019-05-29 12:44:28 +0300

    Types

--- src/components/AttractionTypeCard.jsx
+++ src/components/AttractionTypeCard.jsx
@@ -1,42 +1,32 @@
 import React from 'react'
-import Card from "react-bootstrap/Card";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Image from "react-bootstrap/Image";
 import {connect} from "react-redux";
 import '../css/common.css'
 import {Link} from "react-router-dom";
+import {MDBCard, MDBCardImage, MDBCardTitle, MDBCol, MDBRow} from "mdbreact";
 
 class AttractionTypeCard extends React.Component {
 
     render() {
         const {title, image, link} = this.props;
         const imagePath = `images/${image}`;
-        const linkPath = `/${link}`
+        const linkPath = `/${link}`;
 
-        var card =
-            <div className="green_hover">
-                <Link to={linkPath}>
-                    <Card border="dark">
-                        <Row>
-                            <Col xs={3}>
-                                <div className="Center-Container padding">
-                                    <Image src={imagePath}/>
-                                    {/*src="images/map_marker_museam.png"*/}
-                                </div>
-                            </Col>
-                            <Col xs={8}>
-                                <div className="Center-Container">
-                                    <Card.Title>{title}</Card.Title>
-                                </div>
-                            </Col>
-                        </Row>
-                    </Card>
-                </Link>
-                <hr style={{border: 0}}/>
-            </div>;
+        return <React.Fragment>
+            <Link to={linkPath}>
+                <MDBCard style={{height: "4rem"}} border>
+                    <MDBRow>
+                        <MDBCol style={{margin: "1rem"}} size={3}>
+                            <MDBCardImage src={imagePath} zoom/>
+                        </MDBCol>
+                        <MDBCol style={{padding: 15}} size={8}>
+                            <MDBCardTitle>{title}</MDBCardTitle>
 
-        return card;
+                        </MDBCol>
+                    </MDBRow>
+                </MDBCard>
+            </Link>
+            <hr style={{border: 0}}/>
+        </React.Fragment>;
     }
 }
 
